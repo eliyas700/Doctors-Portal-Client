a0b7867660f52ec9ca3278a711f8f68776a0d42c
Eliyas Hossain <eliyashossain700@gmail.com>
2022-05-17 23:25:25 +0600

    Get Doctors From Db

--- src/Pages/DashBoard/ManageDoctors.js
+++ src/Pages/DashBoard/ManageDoctors.js
@@ -1,9 +1,21 @@
 import React from "react";
+import { useQuery } from "react-query";
+import Loading from "../Shared/Loading/Loading";
 
 const ManageDoctors = () => {
+  const { data: doctors, isLoading } = useQuery("doctors", () =>
+    fetch("http://localhost:5000/doctors", {
+      headers: {
+        authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+      },
+    }).then((res) => res.json())
+  );
+  if (isLoading) {
+    return <Loading></Loading>;
+  }
   return (
     <div>
-      <h2 className="text-2xl ">Manage Doctors</h2>
+      <h2 className="text-2xl ">Manage Doctors:{doctors.length}</h2>
     </div>
   );
 };
