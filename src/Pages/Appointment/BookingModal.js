a8f11c81004157eb083de48446aca5b032983134
Eliyas Hossain <eliyashossain700@gmail.com>
2022-05-19 11:43:22 +0600

    Send Price to the DB and Collect from DB

--- src/Pages/Appointment/BookingModal.js
+++ src/Pages/Appointment/BookingModal.js
@@ -4,7 +4,7 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import auth from "../../firebase.init";
 import { toast } from "react-toastify";
 const BookingModal = ({ treatment, date, setTreatment, refetch }) => {
-  const { slots, name, _id } = treatment;
+  const { slots, name, _id, price } = treatment;
   const [user] = useAuthState(auth);
   const fomattedDate = format(date, "PP");
   const handleSubmit = (event) => {
@@ -14,6 +14,7 @@ const BookingModal = ({ treatment, date, setTreatment, refetch }) => {
       treatmentId: _id,
       treatment: name,
       slot,
+      price,
       date: fomattedDate,
       patient: user.email,
       patientName: user.displayName,
